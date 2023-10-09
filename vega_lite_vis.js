d51b031b6be0aa4254eccac6bab104872195f060
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-10-09 23:50:19 +0800

    Setup each graphs in visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,24 @@
-var vg_1 = "week9_homework.json";
-vegaEmbed("#homework", vg_1).then(function (result) {
+var vg_1 = "map.json";
+vegaEmbed("#map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "week10_homework.json";
-vegaEmbed("#homework2", vg_2).then(function (result) {
+var vg_2 = "tourism_bubble_plot.json";
+vegaEmbed("#tourism_bubble_plot", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "lockdown_scatter_plot.json";
+vegaEmbed("#lockdown_scatter_plot", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "age_line_chart.json";
+vegaEmbed("#age_line_chart", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "pollution_line_chart.json";
+vegaEmbed("#pollution_line_chart", vg_5).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
